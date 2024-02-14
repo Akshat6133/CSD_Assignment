3a1c35b24b972a3e6c81416f7e912d40169df755
Akshat6133 <152408691+Akshat6133@users.noreply.github.com>
2024-02-14 13:26:10 +0530

    Update Home.js

--- assignment/src/screens/User/Home.js
+++ assignment/src/screens/User/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
   useEffect(() => {
     const loadData = async () => {
       try {
-        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}api/shopData`, {
+        const response = await fetch(`https://csd-assignment-ngjn.vercel.app/api/shopData`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
