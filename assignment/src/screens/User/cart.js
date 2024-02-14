dad49811b80f3569f57af943d44edd149ef81af0
akshat <akshatarora252002@gmail.com>
2024-02-14 10:06:03 +0530

    removing materialui import to resolve error

--- assignment/src/screens/User/cart.js
+++ assignment/src/screens/User/cart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Delete from '@material-ui/icons/Delete'
+// import Delete from '@material-ui/icons/Delete'
 import { useCart, useDispatchCart } from '../../components/User/ContextReducer';
 
 export default function Cart() {
@@ -69,7 +69,12 @@ export default function Cart() {
                 <td>{food.qty}</td>
                 <td>{food.size}</td>
                 <td>{food.price}</td>
-                <td ><button type="button" className="btn p-0"><Delete onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button> </td></tr>
+                <td >
+                  <button type="button" className="btn p-0">
+                    {/* <Delete onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /> */}
+                  </button> 
+                </td>
+                </tr>
             ))}
           </tbody>
         </table>
